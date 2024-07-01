4cc3fee6086780bba6f92f825116ee79c49617d8
Hyunsu Kim <hyunsujk@gmail.com>
2024-07-01 18:40:06 -0500

    Time: 53 ms (88.82%), Space: 51.1 MB (36.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,19 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    const map = new Map()
+
     for(let i=0; i<nums.length; i++){
-        const find = target - nums[i]
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[j] === find){
-                return [i, j]
-            }
+        if(map.has(target - nums[i])){
+            return [map.get(target - nums[i]), i]
+        } else {
+            map.set(nums[i], i)
         }
     }
 };
 
-// input is not sorted
+// use map
 // for i=0
-// find is target - nums[i]
-// for j=i+1
-// if nums[j] is find
-// return [i, j]
+// if map has target-num[i]
+// map get target-num[i]
+// else
+// map set num[i], i
