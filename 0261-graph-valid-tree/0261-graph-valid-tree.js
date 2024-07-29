b95fdc4066cd0f2a5043824ed6f44bac72860f14
Hyunsu Kim <hyunsujk@gmail.com>
2024-07-28 22:53:50 -0500

    Time: 95 ms (7.32%), Space: 57.8 MB (7.05%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {boolean}
+ */
+var validTree = function(n, edges) {
+    const adjList = new Array(n).fill().map(()=>[])
+    for(const [start,end] of edges){
+        adjList[start].push(end)
+        adjList[end].push(start)
+    }
+
+    const visited = new Array(n).fill(0)
+    const parent = new Array(n).fill(-1)
+
+    function helper(node){
+        visited[node] = 1
+        for(const nei of adjList[node]){
+            if(visited[nei] === 0){
+                parent[nei] = node
+                const sub = helper(nei) // check sub result
+                if(sub === false){
+                    return false
+                }
+            } else {
+                if(parent[node] != nei){
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    let component = 0
+
+    for(let i=0; i<n; i++){
+        if(visited[i] === 0){
+            const isTree = helper(i)
+            component++
+            if(component > 1 || !isTree){ // do early return
+                return false
+            }
+        }
+    }
+
+    return true
+    
+};
+
+// tree = no cycle, one component
+// make adj list
+// helper node
+// go through and mark visited and parent
+// if nei is visited and nei is not parent of node
+// return false
+// from 0 til <n 
+// if not visited then helper(i)
+// component ++
+// if component is >1 return false
