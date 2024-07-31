c4f3e2d7ca52954061f63f8bfe15936ea3d780a2
Hyunsu Kim <hyunsujk@gmail.com>
2024-07-31 10:23:45 -0500

    Time: 65 ms (77%), Space: 50.7 MB (46.89%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.js
+++ 0647-palindromic-substrings/0647-palindromic-substrings.js
@@ -6,24 +6,22 @@ var countSubstrings = function(s) {
     let count = 0
     for(let i=0; i<s.length; i++){
         // odd
-        let lPt = i
-        let rPt = i
-        while(lPt >= 0 && rPt < s.length && s[lPt] == s[rPt]){
-            count++
-            lPt--
-            rPt++
-        }
+        count += countPalin(s, i, i)
         // even
-        let lPte = i
-        let rPte = i+1
-        while(lPte >= 0 && rPte < s.length && s[lPte] == s[rPte]){
-            count ++
-            lPte--
-            rPte++
-        }
+        count += countPalin(s, i, i+1)
     }
     return count
 };
+
+function countPalin(s, lPt, rPt){
+    let count = 0
+    while(lPt >= 0 && rPt < s.length && s[lPt] == s[rPt]){
+        count ++
+        lPt--
+        rPt++
+    }
+    return count
+}
 
 // pointer is center
 // if left and right pointer equals count++
