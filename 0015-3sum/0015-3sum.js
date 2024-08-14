97142ecaa3cc8be0618f6ab1a0935d9c4345959e
Hyunsu Kim <hyunsujk@gmail.com>
2024-08-14 00:01:19 -0500

    Time: 155 ms (66.16%), Space: 64.2 MB (92.7%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,41 +3,33 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a,b) => a-b)
+    nums.sort((a,b)=> a-b)
     let res = []
-
     for(let i=0; i<nums.length; i++){
         if(i > 0 && nums[i] === nums[i-1]){
             continue
         }
-
-        let l=i+1
-        let r=nums.length-1
-        while(l<r){
-            const threeSum = nums[i]+nums[l]+nums[r]
-            if(threeSum > 0){
+        let l = i + 1
+        let r = nums.length - 1
+        while(l < r){
+            const sum = nums[i] + nums[l] + nums[r]
+            if(sum > 0){
                 r--
-            } else if(threeSum < 0){
+            } else if(sum < 0){
                 l++
             } else {
                 res.push([nums[i], nums[l], nums[r]])
                 l++
+                r--
                 while(nums[l] == nums[l-1] && l < r){
                     l++
+                }
+                while(nums[r] == nums[r+1] && l < r){
+                    r++
                 }
             }
         }
     }
     return res
+    
 };
-
-// sort the nums
-// use i 0 til < length and do l, r pointers
-// if i is same with previous, continue
-// l is i +1, r is length-1
-// while l < r
-// three sum is i + l + r
-// if sum is bigger than 0 then r --
-// else if sum is less than 0 then l ++
-// else which means is 0, push to result, l++
-// while i is duplicate and <r, l++
