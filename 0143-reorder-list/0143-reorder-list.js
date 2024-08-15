e1100663aeba14aae175e3a06292c32074a6d573
Hyunsu Kim <hyunsujk@gmail.com>
2024-08-14 22:46:05 -0500

    Time: 67 ms (96.89%), Space: 56.1 MB (99.22%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.js
+++ 0143-reorder-list/0143-reorder-list.js
@@ -10,23 +10,26 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function(head) {
+    // find middle
     let slow = head
-    let fast = slow.next
-    while(fast && fast.next) {
+    let fast = head.next
+    while(fast && fast.next){
         slow = slow.next
         fast = fast.next.next
     }
 
+    // reverse the second half
     let second = slow.next
-    slow.next = null
     let prev = null
-
+    slow.next = null // disconnect first and second half
     while(second){
-        let temp = second.next
-        second.next = prev
-        prev = second
-        second = temp
+        let temp = second.next // save the next
+        second.next = prev // reverse the arrow
+        prev = second // update prev
+        second = temp // move over
     }
+
+    // merge
     second = prev
     let first = head
     while(second){
@@ -39,29 +42,3 @@ var reorderList = function(head) {
     }
     
 };
-
-// find middle with slow and fast
-// slow is head and fast is slow.next
-// while fast is not the last one or out of range
-// slow moves one, fast moves two
-// slow is the end of the first half
-
-// reverse the second half
-// second is slow.next
-// disconnect first half from second half // slow.next is null
-// prev is null
-// while second
-// save the list // temp is second.next
-// reverse the arrow // second.next is prev
-// update prev // prev is second
-// update second // second is temp
-// now prev is start of second half
-
-// merge first and second intervally
-// second is prev
-// first is head
-// while second
-// save the next // temp 1 is first.next, temp 2 is second.next
-// connect second to first next // first next is second
-// connect second next with temp 1 // second next is temp 1
-// first is temp 1, second is temp 2
