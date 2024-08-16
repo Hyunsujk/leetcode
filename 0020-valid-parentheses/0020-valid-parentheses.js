bd6e561464980603bdc655a947086b3563f4b253
Hyunsu Kim <hyunsujk@gmail.com>
2024-08-15 23:21:34 -0500

    Time: 63 ms (30.57%), Space: 50.1 MB (55.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -3,35 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    if(s.length === 1){
-        return false
-    }
-
-    const options = new Map([
-        ["(", ")"], 
-        ["{", "}"], 
+    const brackets = new Map([
+        ["(", ")"],
+        ["{", "}"],
         ["[", "]"]
     ])
-    const openings = Array.from(options.keys())
+    const openings = Array.from(brackets.keys())
     let q = []
     for(let i=0; i<s.length; i++){
-        if(openings.includes(s[i])){
+        if(openings.includes(s[i])){ // if opening bracket
             q.push(s[i])
-        } else { // closing
-            if(!q.length || options.get(q[q.length-1]) !== s[i]){
+        } else { // if closing bracket
+            if(!q.length || brackets.get(q[q.length-1]) != s[i]){ // if no opening left or not matching
                 return false
             }
             q.pop()
         }
     }
-    return !q.length
+    return !q.length // if length is not 0 extra openings    
 };
 
-// base case
-// if length is 1 return false
-// build dict (:), {:}, [:]
-// get list of opening // dict.keys() 
-// q = []
-// iterate through s, if it's in opening list, push to q,
-// if not in opening list, which means closing, pop from q, check if dict(pop) doesn't match, return false
-// global return true
+// opening left
+// opening and closing are matching
+// closing left
