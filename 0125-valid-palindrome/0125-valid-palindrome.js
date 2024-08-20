7f6e856524a716352e42f0f67a9c24d93adc10c1
Hyunsu Kim <hyunsujk@gmail.com>
2024-08-19 23:00:07 -0500

    Time: 61 ms (67.09%), Space: 51.1 MB (89.71%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,6 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const cleaned = s.replace(/[^a-z0-9]/gi, "").toLowerCase()
-    return cleaned === cleaned.split("").reverse().join("")
+    const cleaned = s.replace(/[^a-z0-9]/gi,"").toLowerCase()
+    
+    let l = 0
+    let r = cleaned.length-1
+    
+    while(l<r){
+        if(cleaned.charAt(l) !== cleaned.charAt(r)){
+            return false
+        }
+        l++
+        r--
+    }
+    return true
 };
