f4006767209ea44ededafd0f3ae0dfca06bdf9a8
Hyunsu Kim <hyunsujk@gmail.com>
2024-08-22 16:33:40 -0500

    Time: 58 ms (16.86%), Space: 48.2 MB (92.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -4,14 +4,9 @@
  */
 var climbStairs = function(n) {
     const steps = [0, 1, 2]
-    
+
     for(let i = 3; i <= n; i++){
-        steps[i % 3] = steps[(i-1) % 3] + steps[(i-2) % 3]
+        steps[i % 3] = steps[(i - 1) % 3] + steps[(i - 2) % 3]
     }
     return steps[n % 3]
 };
-
-//base case
-// n is 0, return 0
-// n is 1, return 1
-// n is 2, return 2
