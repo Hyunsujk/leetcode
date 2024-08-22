b305fc420a9c3d99431f4b632d82b05eb34d6c06
Hyunsu Kim <hyunsujk@gmail.com>
2024-08-21 21:33:21 -0500

    Time: 57 ms (62.09%), Space: 49.4 MB (86.44%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,17 +3,19 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let max = nums[0];
-    let prevMax = nums[0];
-    let prevMin = nums[0];
-    
-    for (let i = 1; i < nums.length; i++) {
-        const options = [nums[i], nums[i] * prevMax, nums[i] * prevMin];
-        prevMax = Math.max(...options);
-        prevMin = Math.min(...options);
-        
-        max = Math.max(max, prevMax);
+    let prevMax = nums[0]
+    let prevMin = nums[0]
+    let max = nums[0]
+
+    for(let i=1; i<nums.length; i++){
+        const currMax = Math.max(prevMax * nums[i], nums[i], prevMin * nums[i])
+        const currMin = Math.min(prevMax * nums[i], nums[i], prevMin * nums[i])
+
+        prevMax = currMax
+        prevMin = currMin
+
+        max = Math.max(max, prevMax)
     }
-    
-    return max;
+    return max
+
 };
