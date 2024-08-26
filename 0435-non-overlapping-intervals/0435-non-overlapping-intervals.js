4b9d9cafe7c42afe1edbb0f7752d940b5f623462
Hyunsu Kim <hyunsujk@gmail.com>
2024-08-25 20:12:47 -0500

    Time: 199 ms (57.42%), Space: 78.6 MB (96.09%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
-    intervals.sort((a,b) => a[0] - b[0])
+    intervals.sort((a,b) => a[1] - b[1])// sort by end time
 
     let count = 0
     let prevEnd = intervals[0][1]
@@ -12,7 +12,7 @@ var eraseOverlapIntervals = function(intervals) {
             prevEnd = intervals[i][1]
         } else {
             count++
-            prevEnd = Math.min(intervals[i][1], prevEnd) // remove longer end
+            prevEnd = Math.min(intervals[i][1], prevEnd) // remove later end
         }
     }
     return count
