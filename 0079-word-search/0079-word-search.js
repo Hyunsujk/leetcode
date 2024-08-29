72d0f447a74c3bc9831560c5ae6b207235458b22
Hyunsu Kim <hyunsujk@gmail.com>
2024-08-29 16:28:31 -0500

    Time: 429 ms (48.2%), Space: 49.1 MB (71.62%) - LeetHub

---
+++ 0079-word-search/0079-word-search.js
@@ -0,0 +1,39 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    const rows = board.length
+    const cols = board[0].length
+
+    function helper(r, c, i){
+        if(i === word.length){
+            return true
+        }
+        if(r < 0 || c < 0 
+        || r >= rows || c >= cols 
+        || board[r][c] !== word[i] 
+        || !board[r][c]){
+            return false
+        }
+
+        const temp = board[r][c]
+        board[r][c] = null
+        const res = helper(r + 1, c, i + 1) 
+                    || helper(r - 1, c, i + 1) 
+                    || helper(r, c + 1, i + 1) 
+                    || helper(r, c - 1, i + 1)
+        board[r][c] = temp
+        return res
+    }
+
+    for(let r = 0; r < rows; r++){
+        for(let c = 0; c < cols; c++){
+            if(helper(r, c, 0)){
+                return true
+            }
+        }
+    }
+    return false
+};
