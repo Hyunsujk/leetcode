a1d3e1bbdc96b3c4d94ac2d74d2a75791c7190ec
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-06 21:24:06 -0500

    Time: 8 ms (69.38%), Space: 17.8 MB (61.65%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        def build_combination(start, slate, total):
+        def build_comb(start, slate, total):
             if total == target:
                 res.append(slate[:])
                 return
-            if total > target:
-                return 
-            for i in range(start, len(candidates)):
-                slate.append(candidates[i])
-                build_combination(i, slate, total + candidates[i])
-                slate.pop()
-        
-        build_combination(0, [], 0)
+            if total > target or start >= len(candidates):
+                return
+            
+            slate.append(candidates[start])
+            build_comb(start, slate, total + candidates[start])
+            slate.pop()
+            build_comb(start + 1, slate, total)
+        build_comb(0, [], 0)
         return res
