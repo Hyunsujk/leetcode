ad7ac3ffc8fd7bcb91d01a6e6ef5d1859c0d74da
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-08 20:16:46 -0500

    Time: 0 ms (100%), Space: 18 MB (15.67%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == "0" or s == "":
+            return 0
+
+        n = len(s)
+        dp = [0] * (n+1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            one_d = int(s[i-1])
+            two_d = int(s[i-2:i])
+
+            if one_d != 0:
+                dp[i] += dp[i-1]
+            
+            if 10 <= two_d <= 26:
+                dp[i] += dp[i-2]
+        
+        return dp[n]
+
+
+        
+        
