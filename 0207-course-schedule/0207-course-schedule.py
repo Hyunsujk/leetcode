bfba22596ba270fb0e6e089bab5a0237a5b10ae8
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-10 12:04:32 -0500

    Time: 4 ms (67.86%), Space: 19 MB (83.36%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,30 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        prereqs = {i: [] for i in range(numCourses)}
+        prereqs = {i : [] for i in range(numCourses)}
+        indegree = [0] * numCourses
+
         for course, prereq in prerequisites:
-            prereqs[course].append(prereq)
+            prereqs[prereq].append(course)
+            indegree[course] += 1
+
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        visited = 0
+
+        while queue:
+            c = queue.popleft()
+            visited += 1
+
+            for p in prereqs[c]:
+                indegree[p] -= 1
+                if indegree[p] == 0:
+                    queue.append(p)
         
-        taking = set()
-        taken = set()
-
-        def noCycle(c):
-            if c in taking:
-                return False
-            if c in taken:
-                return True
-
-            taking.add(c)
-            for prereq in prereqs[c]:
-                if not noCycle(prereq):
-                    return False
-            taking.remove(c)
-            taken.add(c)
-
-            return True
-        
-        for c in range(numCourses):
-            if not noCycle(c):
-                return False
-        return True
-
-        
+        return visited == numCourses
