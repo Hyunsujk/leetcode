216e7382fc90e4db1555d45600f731797bb64fd0
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-10 12:46:28 -0500

    Time: 0 ms (100%), Space: 17.8 MB (38.93%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -9,14 +9,13 @@ class Solution:
 
             for i in range(2, len(houses)):
                 dp[i] = max(dp[i-2] + houses[i], dp[i-1])
-
+            
             return dp[-1]
-
+        
         if len(nums) == 1:
             return nums[0]
         
         first_house = helper(nums[:-1])
         second_house = helper(nums[1:])
 
-        return max(first_house, second_house)
-            
+        return max(first_house, second_house)        
