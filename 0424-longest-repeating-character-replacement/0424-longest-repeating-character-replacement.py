97ae33fc5af4ac087a5e39ac1267fde691553430
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-11 22:15:20 -0500

    Time: 111 ms (84.82%), Space: 17.8 MB (99.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,17 +3,16 @@ class Solution:
         count = defaultdict(int)
         left = 0
         maxFreq = 0
-        length = 0
+        maxLength = 0
 
         for right in range(len(s)):
             count[s[right]] += 1
-            length += 1
             maxFreq = max(maxFreq, count[s[right]])
 
             if (right - left + 1) - maxFreq > k:
                 count[s[left]] -= 1
                 left += 1
-                length -= 1
+            maxLength = max(maxLength, right - left + 1)
 
-        return length 
+        return maxLength 
         
