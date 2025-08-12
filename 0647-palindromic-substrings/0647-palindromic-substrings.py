fa153f47eae6f24fc7b5558b5201806a2a55253b
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-12 10:40:42 -0500

    Time: 239 ms (24.98%), Space: 17.7 MB (65.52%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,17 +1,17 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        res = []
+        self.count = 0
 
         def isPal(l, r):
             if l < 0 or r >= len(s) or s[l] != s[r]:
                 return
-            res.append(s[l:r+1])
+            self.count += 1
             return isPal(l-1, r+1)
         
         for i in range(len(s)):
             isPal(i, i+1)
             isPal(i, i)
 
-        return len(res)
+        return self.count
 
         
