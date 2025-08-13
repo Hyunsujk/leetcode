4eb100651e72c45ba54b8ef4bfe3f80ad56c6825
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-13 14:24:39 -0500

    Time: 148 ms (53.92%), Space: 64.5 MB (6.35%) - LeetHub

---
+++ 1972-rotating-the-box/1972-rotating-the-box.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        rows = len(boxGrid)
+        cols = len(boxGrid[0])
+
+        for r in range(rows):
+            empty = cols - 1
+            for c in range(cols-1, -1, -1):
+                if boxGrid[r][c] == "*":
+                    empty = c-1
+                elif boxGrid[r][c] == "#":
+                    boxGrid[r][c], boxGrid[r][empty] = boxGrid[r][empty], boxGrid[r][c]
+                    empty -= 1
+        
+        output = [[None] * rows for _ in range(cols)]
+
+        for r in range(rows):
+            for c in range(cols):
+                output[c][rows-r-1] = boxGrid[r][c]
+        
+        return output
