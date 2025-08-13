5dd6772849e149919f7a34c542886bcd4b9f4864
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-13 12:31:39 -0500

    Time: 46 ms (72.12%), Space: 17.9 MB (73.4%) - LeetHub

---
+++ 0723-candy-crush/0723-candy-crush.py
@@ -0,0 +1,39 @@
+class Solution:
+    def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
+        rows = len(board)
+        cols = len(board[0])
+
+        while True:
+            crushList = set()
+            for r in range(rows):
+                for c in range(cols-2):
+                    if board[r][c] == board[r][c+1] == board[r][c+2] != 0:
+                        crushList.update([(r,c), (r,c+1), (r,c+2)])
+
+            for c in range(cols):
+                for r in range(rows-2):
+                    if board[r][c] == board[r+1][c] == board[r+2][c] != 0:
+                        crushList.update([(r,c), (r+1,c), (r+2,c)])
+            
+            if not crushList:
+                break
+
+            for r, c in crushList:
+                board[r][c] = 0
+
+            for c in range(cols):
+                lowestZero = rows - 1
+                for r in range(rows-1, -1, -1):
+                    if board[r][c] != 0:
+                        board[lowestZero][c], board[r][c] = board[r][c], board[lowestZero][c]
+                        lowestZero -= 1
+                
+                for r in range(lowestZero, -1, -1):
+                    board[r][c] = 0
+
+        
+        return board
+
+
+
+            
