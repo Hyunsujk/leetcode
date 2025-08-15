f0716da8f881c1c416d00ec641362fbe8dfff263
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-15 13:10:39 -0500

    Time: 173 ms (55.06%), Space: 62.7 MB (72.75%) - LeetHub

---
+++ 1866-restore-the-array-from-adjacent-pairs/1866-restore-the-array-from-adjacent-pairs.py
@@ -0,0 +1,26 @@
+class Solution:
+    def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
+        adjList = defaultdict(list)
+
+        for n1, n2 in adjacentPairs:
+            adjList[n1].append(n2)
+            adjList[n2].append(n1)
+        
+        head = next(k for [k, v] in adjList.items() if len(v) == 1)
+
+        res = [head]
+        visited = set([head])
+
+        while len(res) < len(adjList):
+            last = res[-1]
+            for neigh in adjList[last]:
+                if neigh not in visited:
+                    res.append(neigh)
+                    visited.add(neigh)
+
+        return res
+
+
+        
+
+        
