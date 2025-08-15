66f26b11200e5861aed0359b7e49f9acdb45cf37
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-15 15:09:10 -0500

    Time: 31 ms (72.68%), Space: 17.9 MB (70.63%) - LeetHub

--- 3335-minimum-operations-to-write-the-letter-y-on-a-grid/3335-minimum-operations-to-write-the-letter-y-on-a-grid.py
+++ 3335-minimum-operations-to-write-the-letter-y-on-a-grid/3335-minimum-operations-to-write-the-letter-y-on-a-grid.py
@@ -1,24 +1,22 @@
 class Solution:
     def minimumOperationsToWriteY(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        center = n // 2
-        ypos = set()
-        ycount = [0,0,0]
-        bcount = [0,0,0]
-        
-        for i in range(center):
-            ypos.add((i,i))
-            ycount[grid[i][i]] += 1
-            ypos.add((i, n-1-i))
-            ycount[grid[i][n-1-i]] += 1
-        
-        for i in range(center, n):
-            ypos.add((i, center))
-            ycount[grid[i][center]] += 1
+        m = n // 2
 
+        ypos = set()
+        for i in range(m):
+            ypos.add((i,i))
+            ypos.add((i, n-1-i))
+        for i in range(m, n):
+            ypos.add((i, m))
+        
+        ycount = [0, 0, 0]
+        bcount = [0, 0, 0]
         for x in range(n):
             for y in range(n):
-                if (x,y) not in ypos:
+                if (x, y) in ypos:
+                    ycount[grid[x][y]] += 1
+                else:
                     bcount[grid[x][y]] += 1
         
         ytotal = len(ypos)
@@ -31,19 +29,10 @@ class Solution:
                     continue
                 ychange = ytotal - ycount[ycolor]
                 bchange = btotal - bcount[bcolor]
+
                 change = min(change, ychange + bchange)
-
-
+        
         return change
-
-
-
-        
-
-        
-
-
-
 
 
         
