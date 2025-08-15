04822c602a816339f9d50d342e702a4137d70051
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-14 22:07:30 -0500

    Time: 79 ms (94.67%), Space: 50.1 MB (40.06%) - LeetHub

--- 2779-number-of-adjacent-elements-with-the-same-color/2779-number-of-adjacent-elements-with-the-same-color.py
+++ 2779-number-of-adjacent-elements-with-the-same-color/2779-number-of-adjacent-elements-with-the-same-color.py
@@ -1,32 +1,27 @@
 class Solution:
     def colorTheArray(self, n: int, queries: List[List[int]]) -> List[int]:
-        if not queries or len(queries) == 1:
+        if not queries and len(queries) == 1:
             return [0]
-        
-        answer = [0] * len(queries)
+
+        answer = []
         colors = [0] * n
+        adjacent = 0
 
-        for i, queries in enumerate(queries):
-            colorIdx = queries[0]
-            color = queries[1]
-            oldColor = colors[colorIdx]
-            colors[colorIdx] = color
+        for i, color in queries:
+            c = colors[i]
+            if c != 0:
+                if i-1 >= 0 and colors[i-1] == c:
+                    adjacent -= 1
+                if i+1 < n and colors[i+1] == c:
+                    adjacent -= 1
+            
+            colors[i] = color
 
-            left = colorIdx - 1
-            right = colorIdx +1
-            a = answer[i-1] if i >= 1 else 0
-            if left >= 0:
-                if oldColor != 0 and colors[left] == oldColor:
-                    a -= 1
-                if colors[left] == color:
-                    a += 1
-            if right < n:
-                if oldColor != 0 and colors[right] == oldColor:
-                    a -= 1
-                if colors[right] == color:
-                    a += 1
-            answer[i] = a
+            if i-1 >= 0 and colors[i-1] == color:
+                adjacent += 1
+            if i+1 < n and colors[i+1] == color:
+                adjacent += 1
+            answer.append(adjacent)
         
         return answer
-
         
