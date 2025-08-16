a6b9469320324816dd824d18c222484e0673788b
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-15 20:09:07 -0500

    Time: 151 ms (52.41%), Space: 64.1 MB (8.79%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.py
+++ 1972-rotating-the-box/1972-rotating-the-box.py
@@ -3,19 +3,19 @@ class Solution:
         rows = len(boxGrid)
         cols = len(boxGrid[0])
 
-        for r in range(rows):
-            empty = cols - 1
-            for c in range(cols-1, -1, -1):
-                if boxGrid[r][c] == "*":
-                    empty = c-1
-                elif boxGrid[r][c] == "#":
-                    boxGrid[r][c], boxGrid[r][empty] = boxGrid[r][empty], boxGrid[r][c]
-                    empty -= 1
+        for i in range(rows):
+            lowestEmpty = cols-1
+            for j in range(cols-1, -1, -1):
+                if boxGrid[i][j] == "#":
+                    boxGrid[i][j], boxGrid[i][lowestEmpty] = boxGrid[i][lowestEmpty], boxGrid[i][j]
+                    lowestEmpty -= 1
+                if boxGrid[i][j] == "*":
+                    lowestEmpty = j-1
         
-        output = [[None] * rows for _ in range(cols)]
+        res = [["."] * rows for _ in range(cols)]
 
         for r in range(rows):
             for c in range(cols):
-                output[c][rows-1-r] = boxGrid[r][c]
+                res[c][rows-1-r] = boxGrid[r][c]
         
-        return output
+        return res
