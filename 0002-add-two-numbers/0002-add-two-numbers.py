1af8b651b4cd2e48b78e8bebf151d822ac014f67
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-16 23:45:42 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,23 +5,37 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-
         dummy = ListNode()
-        curr = dummy
-        carryOver = 0
-        while l1 or l2 or carryOver:
-            l1_val = l1.val if l1 else 0
-            l2_val = l2.val if l2 else 0
-            total = l1_val + l2_val + carryOver
+        
+        def add(head: ListNode, left: ListNode, right: ListNode, carry_over: int):
+            left_val = left.val if left else 0
+            right_val = right.val if right else 0
+            total = left_val + right_val + carry_over
             if total >= 10:
-                carryOver = 1
+                node_val = total % 10
+                head.next = ListNode(node_val)
+                head = head.next
+                if left:
+                    left = left.next
+                if right:
+                    right = right.next
+                return add(head, left, right, 1)
+            elif total > 0:
+                node_val = total
+                head.next = ListNode(node_val)
+                head = head.next
+                if left:
+                    left = left.next
+                if right:
+                    right = right.next
+                return add(head, left, right, 0)
             else:
-                carryOver = 0
-            curr.next = ListNode(total % 10)
-            curr = curr.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None 
+                if left and right:
+                    head.next = ListNode(total)
 
-    
+
+        add(dummy, l1, l2, 0)
+
         return dummy.next
+
         
