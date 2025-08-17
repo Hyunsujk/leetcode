526d68e3295da5e65ae037b43f466c300cf02f23
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-16 23:16:34 -0500

    Time: 167 ms (95.83%), Space: 35.2 MB (100%) - LeetHub

--- 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.py
+++ 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.py
@@ -2,24 +2,24 @@ class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
         seen_count = defaultdict(int)
         seen_count[words[0]] = 1
-        prev_lengths = set([len(words[0])])
-
+        seen_lengths = set([len(words[0])])
+        
         total = 0
 
         for i in range(1, len(words)):
             word = words[i]
             curr_length = len(word)
-
-            for prev_length in prev_lengths:
-                if prev_length > curr_length:
+            for prev in seen_lengths:
+                if prev > curr_length:
                     continue
-                elif prev_length == curr_length:
+                elif prev == curr_length:
                     total += seen_count[word]
                 else:
-                    prefix = word[:prev_length]
-                    if prefix == word[curr_length - prev_length:]:
+                    prefix = word[:prev]
+                    if prefix == word[curr_length-prev:]:
                         total += seen_count[prefix]
-            prev_lengths.add(curr_length)
             seen_count[word] += 1
+            seen_lengths.add(curr_length)
+
         return total
-                    
+        
