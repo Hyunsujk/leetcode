22f6fa98b83ff5bc12d54e782ea011da288c1de8
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-16 22:52:06 -0500

    Time: 2262 ms (14.59%), Space: 305.6 MB (7.99%) - LeetHub

--- 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.py
+++ 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.py
@@ -7,40 +7,37 @@ class Trie:
     def __init__(self):
         self.root = TrieNode()
     
-    def add(self, word: str, reverse = False):
-        node = self.root
-
+    def matches(self, word, reverse = False):
         matches = set()
+        node = self.root
         for char in word:
-            if char in node.children:
-                node = node.children[char]
-            else:
-                new_node = TrieNode()
-                node.children[char] = new_node
-                node = new_node
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
             if node.ending_word:
                 matches.add(node.ending_word)
         node.ending_word = word[::-1] if reverse else word
         return matches
-    
 
 
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
         prefixT = Trie()
         suffixT = Trie()
-        
+
         count = defaultdict(int)
         total = 0
-        for w in words:
-            prefixes = prefixT.add(w)
-            rw = w[::-1]
-            suffixes = suffixT.add(rw, True)
+
+        for word in words:
+            prefixes = prefixT.matches(word)
+            reversedWord = word[::-1]
+            suffixes = suffixT.matches(reversedWord, True)
             for p in prefixes:
                 if p in suffixes:
                     total += count[p]
-            count[w] += 1
+            count[word] += 1
         
         return total
+
 
         
