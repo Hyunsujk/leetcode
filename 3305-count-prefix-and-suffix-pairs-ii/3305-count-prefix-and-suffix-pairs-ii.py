53e829add52cc9fd3b1b3e5afc0d072b709894e7
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-17 21:02:09 -0500

    Time: 162 ms (97.92%), Space: 35.3 MB (100%) - LeetHub

--- 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.py
+++ 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.py
@@ -6,21 +6,23 @@ class Solution:
 
         total = 0
 
-        for i in range(1, len(words)):
+        for i in range(1,len(words)):
             word = words[i]
-            curr_length = len(word)
+            curr_l = len(word)
 
-            for prev in seen_length:
-                if prev > curr_length:
+            for prev_l in seen_length:
+                if prev_l > curr_l:
                     continue
-                elif prev == curr_length:
+                elif prev_l == curr_l:
                     total += seen_count[word]
                 else:
-                    prefix = word[:prev]
-                    if prefix == word[curr_length-prev:]:
+                    prefix = word[:prev_l]
+                    if prefix == word[curr_l - prev_l:]:
                         total += seen_count[prefix]
+            seen_length.add(curr_l)    
+            seen_count[word] += 1       
 
-            seen_length.add(curr_length)
-            seen_count[word] += 1
         
         return total
+
+        
