4942a7682427ae1e24e8521b0a872a9fbbb8d834
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-17 22:31:13 -0500

    Time: 2243 ms (14.59%), Space: 305.4 MB (9.03%) - LeetHub

--- 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.py
+++ 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.py
@@ -1,28 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_word = ""
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def match(self, word: str, reverse = False):
+        node = self.root
+        matches = set()
+
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+            if node.end_word:
+                matches.add(node.end_word)
+        node.end_word = word[::-1] if reverse else word
+
+        return matches
+
+
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
-        seen_count = defaultdict(int)
-        seen_count[words[0]] = 1
-        seen_length = set([len(words[0])])
-
+        prefixT = Trie()
+        suffixT = Trie()
         total = 0
 
-        for i in range(1,len(words)):
-            word = words[i]
-            curr_l = len(word)
-
-            for prev_l in seen_length:
-                if prev_l > curr_l:
-                    continue
-                elif prev_l == curr_l:
-                    total += seen_count[word]
-                else:
-                    prefix = word[:prev_l]
-                    if prefix == word[curr_l - prev_l:]:
-                        total += seen_count[prefix]
-            seen_length.add(curr_l)    
-            seen_count[word] += 1       
-
+        count = defaultdict(int)
         
+        for word in words:
+            prefixes = prefixT.match(word)
+            reversedw = word[::-1]
+            suffixes = suffixT.match(reversedw, True)
+            for p in prefixes:
+                if p in suffixes:
+                    total += count[p]
+            count[word] += 1
+            
         return total
+
 
         
