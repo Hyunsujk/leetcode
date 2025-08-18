70d02b7b66084d5c35080395d1b6a6293322b72a
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-17 21:17:53 -0500

    Time: 3 ms (85.33%), Space: 18 MB (42.36%) - LeetHub

--- 3269-number-of-subarrays-that-match-a-pattern-i/3269-number-of-subarrays-that-match-a-pattern-i.py
+++ 3269-number-of-subarrays-that-match-a-pattern-i/3269-number-of-subarrays-that-match-a-pattern-i.py
@@ -1,18 +1,21 @@
 class Solution:
     def countMatchingSubarrays(self, nums: List[int], pattern: List[int]) -> int:
-        numPattern = []
-
+        curr_pattern = []
+        
         for i in range(1, len(nums)):
-            if nums[i] > nums[i-1]:
-                numPattern.append(1)
-            if nums[i] == nums[i-1]:
-                numPattern.append(0)
-            if nums[i] < nums[i-1]:
-                numPattern.append(-1)
+            prev = nums[i-1]
+            curr = nums[i]
+            if curr > prev:
+                curr_pattern.append(1)
+            elif curr == prev:
+                curr_pattern.append(0)
+            else:
+                curr_pattern.append(-1)
 
         count = 0
-        for start in range(len(numPattern) - len(pattern) + 1):
-            if numPattern[start: start+len(pattern)] == pattern:
+        for i in range(len(curr_pattern)-len(pattern)+1):
+            if pattern == curr_pattern[i:i+len(pattern)]:
                 count += 1
-        
+
         return count
+        
