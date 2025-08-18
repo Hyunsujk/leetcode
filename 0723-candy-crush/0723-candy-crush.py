212c5172e2a199697f29c6e14191cc7cc93ed113
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-17 20:13:45 -0500

    Time: 47 ms (68.04%), Space: 18.2 MB (8.34%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.py
+++ 0723-candy-crush/0723-candy-crush.py
@@ -4,31 +4,35 @@ class Solution:
         cols = len(board[0])
 
         while True:
-            crushList = set()
+            crush = set()
+            
             for r in range(rows):
                 for c in range(cols-2):
                     if board[r][c] == board[r][c+1] == board[r][c+2] != 0:
-                        crushList.update({(r,c), (r,c+1), (r,c+2)})
+                        crush.update([(r,c), (r,c+1), (r,c+2)])
             
             for c in range(cols):
                 for r in range(rows-2):
                     if board[r][c] == board[r+1][c] == board[r+2][c] != 0:
-                        crushList.update({(r,c), (r+1,c), (r+2,c)})
-
-            if not crushList:
+                        crush.update([(r,c), (r+1,c), (r+2,c)])
+            
+            if not crush:
                 break
             
-            for r, c in crushList:
+            for r, c in crush:
                 board[r][c] = 0
 
             for c in range(cols):
-                lowestZero = rows - 1
-                for r in range(rows-1, -1, -1):
+                lowestZero = rows-1
+                for r in range(rows-1,-1,-1):
                     if board[r][c] != 0:
                         board[r][c], board[lowestZero][c] = board[lowestZero][c], board[r][c]
                         lowestZero -= 1
-                for r in range(lowestZero, -1, -1):
+                for r in range(lowestZero,-1,-1):
                     board[r][c] = 0
-        
+
         return board
+
+
+
         
