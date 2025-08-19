a7d4f69408177023854eb71cf930e0d60236dd26
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-18 22:59:18 -0500

    Time: 0 ms (100%), Space: 17.8 MB (54.48%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -6,23 +6,19 @@ class Solution:
         while right < len(path):
             while right < len(path) and path[right] != "/":
                 right += 1
-            if right - left == 1:
-                right += 1
-                left += 1
-            else:
+            
+            if right - left > 1:
                 p = path[left+1:right]
-                if p == ".":
-                    left = right
-                    right += 1
-                elif p == "..":
+                if p == "..":
                     if stack:
-                        stack.pop() 
-                    left = right
-                    right += 1
-                else:
-                    stack.append(p)  
-                    left = right
-                    right += 1
+                        stack.pop()
+                elif p != ".":
+                    stack.append(p)
+            
+            left = right
+            right += 1
+
+        
         
         return "/" + "/".join(stack)
                
