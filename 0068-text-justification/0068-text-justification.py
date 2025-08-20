da713dd12ae0875aec06d73c779014ce9f2752fd
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-19 22:27:47 -0500

    Time: 0 ms (100%), Space: 18 MB (38.82%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,33 +1,38 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         res = []
-
         def helper(i, length, line):
             if i == len(words):
                 line_str = " ".join(line)
                 line_str += " " * (maxWidth - len(line_str))
                 res.append(line_str)
                 return
-
-            spaces = len(line)
-            if length + spaces + len(words[i]) > maxWidth:
+            
+            if length + len(line) + len(words[i]) > maxWidth:
                 space_count = len(line) - 1
                 if space_count == 0:
                     line_str = line[0] + " " * (maxWidth - len(line[0]))
                     res.append(line_str)
                 else:
-                    total_space = maxWidth - sum(len(w) for w in line)
-                    even_space, extra_space = divmod(total_space, space_count)
+                    total_spaces = maxWidth - length
+                    even_spaces, extra_spaces = divmod(total_spaces, space_count)
                     line_str = ""
                     for j in range(space_count):
-                        line_str += line[j] + (" " * (even_space + (1 if j < extra_space else 0)))
+                        line_str += line[j] + " " * (even_spaces + (1 if j < extra_spaces else 0))
                     line_str += line[-1]
                     res.append(line_str)
-
                 return helper(i+1, len(words[i]), [words[i]])
             else:
                 line.append(words[i])
                 return helper(i+1, length + len(words[i]), line)
+            
+            
         
         helper(0, 0, [])
+
         return res
+
+            
+
+
+        
