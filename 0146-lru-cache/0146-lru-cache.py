d1a81f324cc6c1a6374c3ac033abd959f1c76198
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-20 23:41:21 -0500

    Time: 167 ms (26.6%), Space: 78 MB (75.91%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,20 +1,21 @@
 class Node:
-    def __init__(self, key: int, val: int):
+    def __init__(self, key = 0, value = 0):
         self.key = key
-        self.val = val
+        self.val = value
         self.prev = None
         self.next = None
+
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
+        self.head = Node()
+        self.tail = Node()
         self.head.next = self.tail
         self.tail.prev = self.head
-
+    
     def _add(self, node):
         last = self.tail.prev
         last.next = node
@@ -28,30 +29,33 @@ class LRUCache:
         prevN.next = nextN
         nextN.prev = prevN
         
-        
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
-            self._remove(node)
-            self._add(node)
-            return node.val
-        else:
+        if key not in self.cache:
             return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._add(node)
+        return node.val
+
         
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             node = self.cache[key]
             self._remove(node)
-        node = Node(key, value)
-        self._add(node)
-        self.cache[key] = node
-
+            node.val = value
+            self._add(node)
+        else:
+            node = Node(key, value)
+            self._add(node)
+            self.cache[key] = node
+        
         if len(self.cache) > self.capacity:
-            oldestN = self.head.next
-            self._remove(oldestN)
-            del self.cache[oldestN.key]
+            lru = self.head.next
+            self._remove(lru)
+            del self.cache[lru.key]
+
         
 
 
