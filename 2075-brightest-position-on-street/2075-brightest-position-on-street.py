f39d7e330184e861cdbf31909203ff629ba315e4
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-22 23:13:04 -0500

    Time: 337 ms (33.39%), Space: 75.7 MB (12.97%) - LeetHub

--- 2075-brightest-position-on-street/2075-brightest-position-on-street.py
+++ 2075-brightest-position-on-street/2075-brightest-position-on-street.py
@@ -1,20 +1,20 @@
 class Solution:
     def brightestPosition(self, lights: List[List[int]]) -> int:
-        brightness_change = defaultdict(int)
+        changes = defaultdict(int)
 
-        for position, radius in lights:
-            brightness_change[position - radius] += 1
-            brightness_change[position + radius + 1] -= 1
-
-        brightest_index = 0
-        brightest = float("-inf")
-        current_brightness = 0
-            
-        for curr, change in sorted(brightness_change.items()):
-            current_brightness += change
-            if current_brightness > brightest:
-                brightest_index, brightest = curr, current_brightness
+        for p, r in lights:
+            changes[p-r] += 1
+            changes[p+r+1] -= 1
         
-        return brightest_index
+        current_brightness = 0
+        brightest_idx = 0
+        brightest = float("-inf")
+
+        for cp, c in sorted(changes.items()):
+            current_brightness += c
+            if current_brightness > brightest:
+                brightest, brightest_idx = current_brightness, cp
+        
+        return brightest_idx
 
         
