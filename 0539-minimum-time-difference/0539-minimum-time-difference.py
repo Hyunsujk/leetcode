57b4a6bfe3f65d160e7f45828e9e617a2d4ce3ca
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-23 12:55:37 -0500

    Time: 3 ms (93.77%), Space: 19.9 MB (64.89%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.py
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -3,20 +3,22 @@ class Solution:
         if len(set(timePoints)) < len(timePoints):
             return 0
         
-        for i in range(len(timePoints)):
-            time = timePoints[i]
-            hour = int(time[0:2])
-            minute = int(time[3:5])
+        minutes = []
+        for t in timePoints:
+            hour = int(t[:2])
+            minute = int(t[3:])
+            minutes.append(hour * 60 + minute)
+        
+        minutes.sort()
+        print(minutes)
 
-            minutes = hour * 60 + minute
-            timePoints[i] = minutes
+        minDiff = 24*60
+        for i in range(1, len(minutes)):
+            if minutes[i] - minutes[i-1] < minDiff:
+                minDiff = minutes[i] - minutes[i-1]
         
-        timePoints.sort()
-        minDiff = float("inf")
-        for i in range(1, len(timePoints)):
-            diff = timePoints[i] - timePoints[i-1]
-            minDiff = min(minDiff, diff)
-        
-        minDiff = min(minDiff, 24 * 60 - (timePoints[-1] - timePoints[0]))
+        minDiff = min(minDiff, (24*60 - (minutes[-1] - minutes[0])))
 
         return minDiff
+
+        
