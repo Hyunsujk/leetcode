79e68d5cc4d53d46657eb671861e7ab8a7d35bc0
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-23 23:18:20 -0500

    Time: 473 ms (81.25%), Space: 28.1 MB (19.11%) - LeetHub

--- 2889-number-of-black-blocks/2889-number-of-black-blocks.py
+++ 2889-number-of-black-blocks/2889-number-of-black-blocks.py
@@ -1,23 +1,19 @@
 class Solution:
     def countBlackBlocks(self, m: int, n: int, coordinates: List[List[int]]) -> List[int]:
-        subblock_set = defaultdict(int)
+        rows = m
+        cols = n
+        counts = defaultdict(int)
 
-        for [x, y] in coordinates:
-            for x_change in [0, -1]:
-                for y_change in [0, -1]:
-                    top_row = x + x_change
-                    top_col = y + y_change
-                    if 0 <= top_row < m - 1 and 0 <= top_col < n - 1:
-                        subblock_set[(top_row, top_col)] += 1
+        for r, c in coordinates:
+            for rc, cc in [(-1, 0), (0, 0), (0, -1), (-1, -1)]:
+                nr = r + rc
+                nc = c + cc
+                if 0 <= nr < rows - 1 and 0 <= nc < cols - 1:
+                    counts[(nr, nc)] += 1
         
         res = [0] * 5
-        for count in subblock_set.values():
-            res[count] += 1
-        
-        res[0] = (m-1)*(n-1) - sum(res[1:])
-
+        for c in counts.values():
+            res[c] += 1
+        totalBlocks = (rows-1) * (cols-1)
+        res[0] = totalBlocks - sum(res[1:])
         return res
-
-
-
-        
