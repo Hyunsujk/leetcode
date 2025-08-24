4a1dbba00cc18bdc953a7890d1892365ea9b2e28
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-24 12:08:21 -0500

    Time: 47 ms (67.83%), Space: 18.2 MB (29.7%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.py
+++ 0723-candy-crush/0723-candy-crush.py
@@ -2,38 +2,36 @@ class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
         rows = len(board)
         cols = len(board[0])
-        
+
         while True:
             crushable = set()
+
             for r in range(rows-2):
                 for c in range(cols):
                     if board[r][c] == board[r+1][c] == board[r+2][c] != 0:
-                        crushable.update([(r,c), (r+1, c), (r+2, c)])
+                        crushable.update([(r, c), (r+1, c), (r+2, c)])
+            
             for r in range(rows):
                 for c in range(cols-2):
                     if board[r][c] == board[r][c+1] == board[r][c+2] != 0:
-                        crushable.update([(r,c), (r, c+1), (r, c+2)])
-
+                        crushable.update([(r, c), (r, c+1), (r, c+2)])
+            
             if not crushable:
                 break
             
             for r, c in crushable:
                 board[r][c] = 0
-
+            
             for c in range(cols):
-                lowestZero = rows-1
+                lowestZero = rows - 1
                 for r in range(rows-1,-1,-1):
                     if board[r][c] != 0:
                         board[r][c], board[lowestZero][c] = board[lowestZero][c], board[r][c]
                         lowestZero -= 1
-                for i in range(lowestZero, -1, -1):
-                    board[i][c] = 0
-                    
+                
+                for r in range(lowestZero,-1,-1):
+                    board[r][c] = 0
         
         return board
-                    
-                    
-
-
 
         
