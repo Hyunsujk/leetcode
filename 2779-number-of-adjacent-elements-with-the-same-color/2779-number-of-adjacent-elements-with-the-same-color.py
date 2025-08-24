e30739b856aeb8f9de6f3cf9d13f62e5ef710a0b
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-23 23:28:36 -0500

    Time: 89 ms (76.26%), Space: 50 MB (64.8%) - LeetHub

--- 2779-number-of-adjacent-elements-with-the-same-color/2779-number-of-adjacent-elements-with-the-same-color.py
+++ 2779-number-of-adjacent-elements-with-the-same-color/2779-number-of-adjacent-elements-with-the-same-color.py
@@ -7,17 +7,19 @@ class Solution:
         for i, c in queries:
             oldColor = colors[i]
 
-            if i-1 >= 0 and colors[i-1] == oldColor != 0:
-                matches -= 1
-            if i+1 < len(colors) and colors[i+1] == oldColor != 0:
-                matches -= 1
+            if i-1 >= 0:
+                if colors[i-1] == oldColor != 0:
+                    matches -= 1
+                if colors[i-1] == c:
+                    matches += 1
+            if i+1 < len(colors):
+                if colors[i+1] == oldColor != 0:
+                    matches -= 1
+                if colors[i+1] == c:
+                    matches += 1
             
             colors[i] = c
 
-            if i-1 >= 0 and colors[i-1] == c:
-                matches += 1
-            if i+1 < len(colors) and colors[i+1] == c:
-                matches += 1
             output.append(matches)
         
         return output
