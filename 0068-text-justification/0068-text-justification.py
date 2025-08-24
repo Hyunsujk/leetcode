da22fecd368097ca6fb36008cc7506fc767dd788
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-24 12:00:05 -0500

    Time: 0 ms (100%), Space: 17.9 MB (38.68%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -8,25 +8,26 @@ class Solution:
                 subline += " " * (maxWidth - len(subline))
                 res.append(subline)
                 return
-
-            currWord = words[i]
-            if length + len(line) + len(currWord) > maxWidth:
-                spaceSection = len(line)-1
-                spaces = maxWidth - length
-                if spaceSection == 0:
-                    subline = line[0] + " " * spaces
+            currW = words[i]
+            totalSpaces = maxWidth - length
+            if length + len(line) + len(currW) > maxWidth:
+                spaceSections = len(line)-1
+                if spaceSections == 0:
+                    subline = line[0]
+                    subline += " " * totalSpaces
                 else:
-                    evenSpaces, extraSpaces = divmod(spaces, spaceSection)
+                    even, extra = divmod(totalSpaces, spaceSections)
                     subline = ""
-                    for j in range(spaceSection):
-                        subline += line[j] + " " * (evenSpaces + (1 if j < extraSpaces else 0))
+                    for j in range(spaceSections):
+                        subline += line[j] + " " * (even + (1 if j < extra else 0))
                     subline += line[-1]
                 res.append(subline)
-                return helper(i+1, [currWord], len(currWord))
+                return helper(i+1, [currW], len(currW))
             else:
-                line.append(currWord)
-                return helper(i+1, line, length + len(currWord))
-            
+                line.append(currW)
+                return helper(i+1, line, length + len(currW))
+        
         helper(0, [], 0)
         return res
+
         
