a5e62e43f1a6338775ba3ebebeb8fb678f65c06f
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-24 13:44:52 -0500

    Time: 3 ms (37.87%), Space: 17.8 MB (83.01%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -1,24 +1,23 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        if path[0] != "/":
-            return ""
         left = 0
         right = 1
-        p = []
+
+        simplified = []
 
         while right <= len(path):
             if right < len(path) and path[right] != "/":
                 right += 1
             else:
-                word = path[left+1:right]
-                if word == "..":
-                    if p:
-                        p.pop()
-                elif word != "." and word != "":
-                    p.append(word)
+                p = path[left+1:right]
+                if p == "..":
+                    if simplified:
+                        simplified.pop()
+                elif p != "." and p != "":
+                    simplified.append(p)
                 left = right
                 right += 1
         
-        return "/" + "/".join(p)
-
+        return "/" + "/".join(simplified)
+                        
         
