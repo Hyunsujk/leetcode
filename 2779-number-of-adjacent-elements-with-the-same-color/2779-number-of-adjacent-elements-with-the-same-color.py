77482696f244f1177e4232a6a9bbdea0f74a8cd6
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-23 23:27:34 -0500

    Time: 129 ms (26.47%), Space: 50.1 MB (39.43%) - LeetHub

--- 2779-number-of-adjacent-elements-with-the-same-color/2779-number-of-adjacent-elements-with-the-same-color.py
+++ 2779-number-of-adjacent-elements-with-the-same-color/2779-number-of-adjacent-elements-with-the-same-color.py
@@ -1,28 +1,24 @@
 class Solution:
     def colorTheArray(self, n: int, queries: List[List[int]]) -> List[int]:
-        if not queries and len(queries) == 1:
-            return [0]
-
-        answer = []
         colors = [0] * n
-        adjacent = 0
 
-        for i, color in queries:
-            c = colors[i]
-            if c != 0:
-                if i-1 >= 0 and colors[i-1] == c:
-                    adjacent -= 1
-                if i+1 < n and colors[i+1] == c:
-                    adjacent -= 1
+        output = []
+        matches = 0
+        for i, c in queries:
+            oldColor = colors[i]
+
+            if i-1 >= 0 and colors[i-1] == oldColor != 0:
+                matches -= 1
+            if i+1 < len(colors) and colors[i+1] == oldColor != 0:
+                matches -= 1
             
-            colors[i] = color
+            colors[i] = c
 
-    
-            if i-1 >= 0 and colors[i-1] == color:
-                adjacent += 1
-            if i+1 < n and colors[i+1] == color:
-                adjacent += 1
-            answer.append(adjacent)
+            if i-1 >= 0 and colors[i-1] == c:
+                matches += 1
+            if i+1 < len(colors) and colors[i+1] == c:
+                matches += 1
+            output.append(matches)
         
-        return answer
+        return output
         
