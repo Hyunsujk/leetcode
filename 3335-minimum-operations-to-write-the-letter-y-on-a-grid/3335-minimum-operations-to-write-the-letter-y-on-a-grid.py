c103586b2322fd89ebb21f2f90504ba145838b5f
Hyunsu Kim <hyunsujk@gmail.com>
2025-08-23 22:35:49 -0500

    Time: 47 ms (43.09%), Space: 18.2 MB (10.22%) - LeetHub

--- 3335-minimum-operations-to-write-the-letter-y-on-a-grid/3335-minimum-operations-to-write-the-letter-y-on-a-grid.py
+++ 3335-minimum-operations-to-write-the-letter-y-on-a-grid/3335-minimum-operations-to-write-the-letter-y-on-a-grid.py
@@ -1,38 +1,30 @@
 class Solution:
     def minimumOperationsToWriteY(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        m = n // 2
+        mid = n // 2
+        y = set()
 
-        ypos = set()
-        for i in range(m):
-            ypos.add((i,i))
-            ypos.add((i, n-1-i))
-        for i in range(m, n):
-            ypos.add((i, m))
+        for i in range(mid+1):
+            y.update([(i,i), (i,n-1-i), (n-1-i, mid)])
         
-        ycount = [0, 0, 0]
-        bcount = [0, 0, 0]
-        for x in range(n):
-            for y in range(n):
-                if (x, y) in ypos:
-                    ycount[grid[x][y]] += 1
+        yCount = {0:0, 1:0, 2:0}
+        notYCount = {0:0, 1:0, 2:0}
+
+        for r in range(n):
+            for c in range(n):
+                if (r,c) in y:
+                    yCount[grid[r][c]] += 1
                 else:
-                    bcount[grid[x][y]] += 1
+                    notYCount[grid[r][c]] += 1
         
-        ytotal = len(ypos)
-        btotal = n*n - ytotal
+        ytotal = len(y)
+        y0 = ytotal - yCount[0] + min((notYCount[0]+notYCount[2]), (notYCount[0]+notYCount[1]))
+        y1 = ytotal - yCount[1] + min((notYCount[1]+notYCount[0]), (notYCount[1]+notYCount[2]))
+        y2 = ytotal - yCount[2] + min((notYCount[0]+notYCount[2]), (notYCount[1]+notYCount[2]))
         
-        change = float("inf")
-        for ycolor in range(3):
-            for bcolor in range(3):
-                if ycolor == bcolor:
-                    continue
-                ychange = ytotal - ycount[ycolor]
-                bchange = btotal - bcount[bcolor]
+        return min([y0, y1, y2])
 
-                change = min(change, ychange + bchange)
-        
-        return change
+
 
 
         
