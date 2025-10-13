78303de581dd20bf5bec6955911f636430a0b102
Hyunsu Kim <hyunsujk@gmail.com>
2025-10-12 23:57:49 -0500

    Time: 0 ms (100%), Space: 17.6 MB (81.21%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -12,11 +12,6 @@ class Solution:
         q = [root]
         while q:
             node = q.pop(0)
-            node.left, node.right = node.right, node.left
-
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
+            node.left, node.right = self.invertTree(node.right), self.invertTree(node.left)
         
         return root
