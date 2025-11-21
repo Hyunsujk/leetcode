60639af1c668fb4507e7b293a998dd1f5aa33e65
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-20 22:20:27 -0600

    Time: 3 ms (92.11%), Space: 17.8 MB (55.35%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -6,18 +6,15 @@ class Solution:
         def helper(start, slate, total):
             if total == target:
                 res.append(slate[:])
-                return
-            
+                return 
             for i in range(start, len(candidates)):
                 if i > start and candidates[i] == candidates[i-1]:
                     continue
-                
                 if total + candidates[i] > target:
                     break
-                
                 slate.append(candidates[i])
                 helper(i+1, slate, total + candidates[i])
                 slate.pop()
-
+        
         helper(0, [], 0)
         return res
