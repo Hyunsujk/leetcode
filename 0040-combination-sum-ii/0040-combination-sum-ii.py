d588743b04a6c52107daec3fe9ef52f7d2e468fa
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-20 22:11:14 -0600

    Time: 19 ms (61.28%), Space: 17.7 MB (97.21%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+
+        def helper(start, slate, total):
+            if total == target:
+                res.append(slate[:])
+                return
+            
+            if total > target:
+                return
+            
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+                
+                slate.append(candidates[i])
+                helper(i+1, slate, total + candidates[i])
+                slate.pop()
+
+        helper(0, [], 0)
+        return res
