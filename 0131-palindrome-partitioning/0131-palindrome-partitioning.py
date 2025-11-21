ff947a8503a81e991ec8c3245831714fc1d09e0b
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-20 23:35:39 -0600

    Time: 49 ms (55.27%), Space: 34.4 MB (73.81%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -3,15 +3,15 @@ class Solution:
         res = []
 
         def dfs(start, slate):
-            if start == len(s) and slate:
+            if start == len(s):
                 res.append(slate[:])
-
+                return
+            
             for end in range(start, len(s)):
-                sub_str = s[start: end+1]
-                if sub_str == sub_str[::-1]:
-                    slate.append(sub_str)
+                sub = s[start:end+1]
+                if sub == sub[::-1]:
+                    slate.append(sub)
                     dfs(end+1, slate)
                     slate.pop()
-        
         dfs(0, [])
         return res
