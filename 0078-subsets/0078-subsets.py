b8a74b6a8b76d4b11756fac8fc971b74f928ce37
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-20 21:39:37 -0600

    Time: 0 ms (100%), Space: 18 MB (21.01%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -4,13 +4,13 @@ class Solution:
         
         def helper(i, slate):
             if i == len(nums):
-                self.res.append(slate)
+                self.res.append(slate[:])
                 return
             
             slate.append(nums[i])
-            helper(i+1, slate[:])
+            helper(i+1, slate)
             slate.pop()
-            helper(i+1, slate[:])
+            helper(i+1, slate)
         
         helper(0, [])
         return self.res
