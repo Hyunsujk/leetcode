37736b8fc10858381706d28006966123831c1064
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-20 22:18:14 -0600

    Time: 3 ms (97.28%), Space: 17.8 MB (60.69%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,19 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+        candidates.sort()
 
         def helper(start, slate, total):
             if total == target:
                 res.append(slate[:])
                 return
             
-            if total > target or start == len(candidates):
-                return
-            
-            slate.append(candidates[start])
-            helper(start, slate, total + candidates[start])
-            slate.pop()
-            helper(start+1, slate, total)
+            for i in range(start, len(candidates)):
+                if total + candidates[i] > target:
+                    break
+                slate.append(candidates[i])
+                helper(i, slate, total + candidates[i])
+                slate.pop()
         
         helper(0, [], 0)
         return res
