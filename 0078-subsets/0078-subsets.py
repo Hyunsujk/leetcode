49c842e732ffcb3df54b4da890a379498150fdb4
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-20 21:38:58 -0600

    Time: 0 ms (100%), Space: 17.8 MB (90.31%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        
+        def helper(i, slate):
+            if i == len(nums):
+                self.res.append(slate)
+                return
+            
+            slate.append(nums[i])
+            helper(i+1, slate[:])
+            slate.pop()
+            helper(i+1, slate[:])
+        
+        helper(0, [])
+        return self.res
