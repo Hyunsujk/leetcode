e954ee462bf8fc9071f78d12c719dfa8be63b69c
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-22 15:49:44 -0600

    Time: 1191 ms (66.68%), Space: 65.3 MB (77.52%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,39 +1,38 @@
 class TrieNode:
-    def __init__(self, char = ""):
-        self.char = char
+    def __init__(self):
         self.children = {}
         self.is_end = False
 
 class WordDictionary:
 
     def __init__(self):
-        self.root = TrieNode()
+        self.trie = TrieNode()
         
+
     def addWord(self, word: str) -> None:
-        node = self.root
-        for char in word:
-            if char in node.children:
-                node = node.children[char]
-            else:
-                new_node = TrieNode(char)
-                node.children[char] = new_node
-                node = new_node
-        node.is_end = True
+        curr = self.trie
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.is_end = True        
 
     def search(self, word: str) -> bool:
-        def dfs(node, i):
+        def dfs(i, node):
             if i == len(word):
                 return node.is_end
-            if word[i] == ".":
+            if word[i] != ".":
+                if word[i] not in node.children:
+                    return False
+                return dfs(i+1, node.children[word[i]])
+            else:
                 for child in node.children.values():
-                    if dfs(child, i+1):
+                    if dfs(i+1, child):
                         return True
                 return False
-            if word[i] not in node.children:
-                return False
-            return dfs(node.children[word[i]], i+1)
-        
-        return dfs(self.root, 0)
+
+        return dfs(0, self.trie)
+
         
 
 
