5b12dd198df5427ba60e83d1a8587c1bc66328d0
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-24 17:54:37 -0600

    Time: 44 ms (49.48%), Space: 18.3 MB (15.4%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -11,7 +11,7 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
-
+        
         visited = {}
 
         def dfs(node):
@@ -27,4 +27,3 @@ class Solution:
             return copy
         
         return dfs(node)
-        
