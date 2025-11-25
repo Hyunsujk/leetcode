6dc1209575c6ea96a44f3f7628c973c2be727dd3
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-25 14:26:19 -0600

    Time: 3 ms (87.87%), Space: 19.8 MB (15.09%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -3,28 +3,25 @@ class Solution:
         adjList = defaultdict(list)
         for c, prereq in prerequisites:
             adjList[c].append(prereq)
-
-        taken = set()
+        
         taking = set()
+        taken = set()
 
-        def dfs(course):
+        def noCycle(course):
             if course in taking:
                 return False
             if course in taken:
                 return True
             
             taking.add(course)
-            
             for prereq in adjList[course]:
-                if not dfs(prereq):
+                if not noCycle(prereq):
                     return False
-
             taking.remove(course)
             taken.add(course)
             return True
-
-
+        
         for i in range(numCourses):
-            if not dfs(i):
+            if not noCycle(i):
                 return False
         return True
