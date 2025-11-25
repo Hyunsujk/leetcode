3796ed92584cce34fc1210f69a8eaf9ef93d7242
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-25 14:49:54 -0600

    Time: 4 ms (47.97%), Space: 19.2 MB (47.95%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,26 +1,23 @@
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:      
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         next_classes = defaultdict(list)
         prereq_count = [0] * numCourses
+
         for c, prereq in prerequisites:
             next_classes[prereq].append(c)
             prereq_count[c] += 1
-
+        
         q = deque([i for i in range(numCourses) if prereq_count[i] == 0])
 
         order = []
-    
+
         while q:
             course = q.popleft()
             order.append(course)
 
-            for next_course in next_classes[course]:
-                prereq_count[next_course] -= 1
-                if prereq_count[next_course] == 0:
-                    q.append(next_course)
+            for next_class in next_classes[course]:
+                prereq_count[next_class] -= 1
+                if prereq_count[next_class] == 0:
+                    q.append(next_class)
         
         return order if len(order) == numCourses else []
-
-                
-
-        
