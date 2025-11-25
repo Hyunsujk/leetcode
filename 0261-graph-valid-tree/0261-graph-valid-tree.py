0f03753cd5561deefe350a6ea9fa53afa05274b3
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-25 15:11:23 -0600

    Time: 5 ms (17.47%), Space: 19.4 MB (27.6%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adjList = defaultdict(list)
+        for a, b in edges:
+            adjList[a].append(b)
+            adjList[b].append(a)
+        
+        visited = set()
+        
+        def dfs(node, parent):
+            visited.add(node)
+            for neigh in adjList[node]:
+                if neigh == parent:
+                    continue
+                if neigh in visited:
+                    return False
+                if not dfs(neigh, node):
+                    return False
+            return True
+        
+        return dfs(0, -1) and len(visited) == n
+
