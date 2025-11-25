707caacd3a81218a94bac67d03b4b67d2f5a5692
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-24 20:43:04 -0600

    Time: 5 ms (66.56%), Space: 22.1 MB (25.11%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -8,30 +8,30 @@ class Solution:
 
         not_surrounded = set()
 
-        def dfs(r, c, visited):
-            if (r,c) in visited:
+        def dfs(r, c):
+            if (r,c) in not_surrounded:
                 return 
-            visited.add((r,c))
+            not_surrounded.add((r,c))
 
             for dr, dc in [(0,1),(0,-1),(1,0),(-1,0)]:
                 nr = r + dr
                 nc = c + dc
                 if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == "O":
-                    dfs(nr, nc, visited)
+                    dfs(nr, nc)
 
         for r in range(rows):
             if board[r][0] == "O":
-                dfs(r, 0, not_surrounded)
+                dfs(r, 0)
             if board[r][cols-1] == "O":
-                dfs(r, cols-1, not_surrounded)
+                dfs(r, cols-1)
         
         for c in range(cols):
             if board[0][c] == "O":
-                dfs(0, c, not_surrounded)
+                dfs(0, c)
             if board[rows-1][c] == "O":
-                dfs(rows-1, c, not_surrounded)
+                dfs(rows-1, c)
         
         for r in range(rows):
             for c in range(cols):
-                if (r,c) not in not_surrounded:
+                if board[r][c] == "O" and (r,c) not in not_surrounded:
                     board[r][c] = "X"
