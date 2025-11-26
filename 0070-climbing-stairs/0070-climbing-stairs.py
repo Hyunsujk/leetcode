5a970fe597be6adaec8775cd54a45595a82d47df
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-25 19:37:54 -0600

    Time: 0 ms (100%), Space: 17.8 MB (29.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,14 +4,11 @@ class Solution:
             return 1
         if n == 2:
             return 2
-            
-        dp = [0] * (n+1)
-        dp[1] = 1
-        dp[2] = 2
+        steps = [0] * (n+1)
+        steps[1] = 1
+        steps[2] = 2
 
         for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+            steps[i] = steps[i-1] + steps[i-2]
         
-        return dp[-1]
-
-        
+        return steps[-1]
