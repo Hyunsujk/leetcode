fea8895812fbebe001bd8122d2158184a2dfe8d8
Hyunsu Kim <hyunsujk@gmail.com>
2025-11-29 17:16:56 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -8,21 +8,22 @@ class Solution:
                 break
             if i > 0 and num == nums[i-1]:
                 continue
+            
             l = i+1
             r = len(nums)-1
-
             while l < r:
-                t = num + nums[l] + nums[r]
-                if t < 0:
-                    l += 1
-                elif t > 0:
+                total = num + nums[l] + nums[r]
+                if total > 0:
                     r -= 1
+                elif total < 0:
+                    l += 1
+                    while nums[l] == nums[l-1]:
+                        l += 1
                 else:
                     res.append([num, nums[l], nums[r]])
                     l += 1
                     r -= 1
                     while l < r and nums[l] == nums[l-1]:
                         l += 1
-
+    
         return res
-        
